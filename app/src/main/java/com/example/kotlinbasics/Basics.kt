6b99bde010d57f451e7aac73ecfb601053115723
Brian Sanchez Aguilar <briansanchezaguilar@gmail.com>
2022-11-05 19:54:21 -0700

    Se agrega base de fundamentos

---
+++ app/src/main/java/com/example/kotlinbasics/Basics.kt
@@ -0,0 +1,27 @@
+package com.example.kotlinbasics
+
+fun main(){
+    // Val para variables
+    var miNombre = "Brian sanchez aguilar"
+    miNombre = "Josue"
+
+    // Val para constantes
+    val nombre2= "juan"
+
+    // TODO: add new functions
+    // No se puede reasignar |
+    // nombre2= "juans"
+
+    // Type string
+
+    // Type int 32 bit
+    var age = 22
+
+    var byte : Byte = 13
+    var short : Short = 125
+    var int : Int = 123123123
+    var long : Long = 120398123094871203
+
+
+    print("Mi nombre es ${miNombre}")
+}
